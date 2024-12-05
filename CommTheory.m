96541c42f7528cc39b142e9434b006fbc4e665d3
mahmoudelfeelig <mahmoudelfeelig@gmail.com>
2024-12-05 22:55:43 +0100

    Adding the quantization step

--- CommTheory.m
+++ CommTheory.m
@@ -27,3 +27,35 @@ for T_s = T_s_values
     % ylabel('Amplitude');
 end
 
+%% Quantization
+
+% Uniform
+% function [quantized_values, levels] = uniform_quantizer(signal, L, A)
+%     levels = linspace(-A, A, L);  % Define L quantization levels
+%     delta = levels(2) - levels(1); % Step size
+%     quantized_values = round((signal + A) / delta) * delta - A; % Quantize
+% end
+
+function [quantized_values, levels] = uniform_quantizer(signal, L, A)
+    % Define L quantization levels linearly spaced between -A and A
+    levels = linspace(-A, A, L);  
+    
+    % Quantize the signal: find the closest level to each sample
+    quantized_values = zeros(size(signal));  % Initialize output array
+    for i = 1:length(signal)
+        % Find the closest quantization level
+        [~, idx] = min(abs(signal(i) - levels));  % Find closest level index
+        quantized_values(i) = levels(idx);  % Assign the quantized value
+    end
+end
+
+% meow :3
+function quantized_values = mu_law_quantizer(signal, L, mu)
+    normalized_signal = signal / max(abs(signal)); % Normalize
+    compressed_signal = sign(normalized_signal) .* log(1 + mu * abs(normalized_signal)) / log(1 + mu);
+    quantized_signal = round((compressed_signal + 1) * (L - 1) / 2); % Uniform quantization
+    quantized_values = 2 * quantized_signal / (L - 1) - 1; % Decompress
+    quantized_values = quantized_values * max(abs(signal)); % Restore scale
+end
+
+
